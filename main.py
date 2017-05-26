114d98da5ff77690871d28d24a7a561a5623a40e
Alvipe <alvipe5@protonmail.com>
2017-05-26 13:04:08 +0200

    Add functions to get EMG and IMU data

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import open_myo as myo
 
 myo_mac_addr = myo.get_myo()
-print(myo_mac_addr)
+print("MAC address: %s" % myo_mac_addr)
 myo_device = myo.Device()
 myo_device.services.set_leds([128, 128, 255], [128, 128, 255])  # purple logo and bar LEDs)
-print(myo_device.services.battery())
-
-# myOjete = myo.MyoDevice()
-# myo.suscribeEmg(myOjete.myo)
-# while True:
-#     myOjete.myo.waitForNotifications(1)
-
-# myOjete = myo.connectMyo(myo.getMyo())
-# fw = myo.firmware(myOjete)
-# print('firmware version: %d.%d.%d.%d' % (fw[0], fw[1], fw[2], fw[3]))
+myo_device.services.vibrate(1) # short vibration
+fw = myo_device.services.firmware()
+print("Firmware version: %d.%d.%d.%d" % (fw[0], fw[1], fw[2], fw[3]))
+batt = myo_device.services.battery()
+print("Battery level: %d" % batt)
+myo_device.services.emg_filt_notifications()
+# myo_device.services.emg_raw_notifications()
+myo_device.services.imu_notifications()
+# myo_device.services.battery_notifications()
+myo_device.services.set_mode(myo.emg_mode.FILT, myo.imu_mode.ALL, myo.classifier_mode.OFF)
+while True:
+    if myo_device.services.waitForNotifications(1):
+        continue
+    print("Waiting...")
--- open_myo.py
+++ open_myo.py
@@ -9,34 +9,52 @@ class Services(btle.Peripheral):
         btle.Peripheral.__init__(self, mac)
         time.sleep(0.5)
 
+    # Get the firmware version
     def firmware(self):
         hex_fw = self.readCharacteristic(0x17)
         fw = struct.unpack('<4h', hex_fw)
         return fw
 
+    # Get the battery level
     def battery(self):
         hex_batt = self.readCharacteristic(0x11)
         batt = ord(hex_batt)
         return batt
 
+    # Change the color of the logo and bar LEDs
     def set_leds(self, logo, line):
-        self.writeCharacteristic(0x19, struct.pack('8B', 6, 6, *(logo + line)))
+        self.writeCharacteristic(0x19, struct.pack('<8B', 6, 6, *(logo + line)))
 
+    def vibrate(self, length):
+        if length in range(1, 4):
+            self.writeCharacteristic(0x19, struct.pack('<3B', 3, 1, length))
+
+    # Suscribe to battery notifications
     def battery_notifications(self):
         self.writeCharacteristic(0x12, b'\x01\x10')
 
-    def emg_notifications(self):
+    # Suscribe to raw EMG notifications
+    def emg_raw_notifications(self):
         self.writeCharacteristic(0x2c, b'\x01\x00')
         self.writeCharacteristic(0x2f, b'\x01\x00')
         self.writeCharacteristic(0x32, b'\x01\x00')
         self.writeCharacteristic(0x35, b'\x01\x00')
 
+    # Suscribe to filtered EMG notifications
+    def emg_filt_notifications(self):
+        self.writeCharacteristic(0x28, b'\x01\x00')
+
+    # Suscribe to IMU notifications
     def imu_notifications(self):
         self.writeCharacteristic(0x1d, b'\x01\x00')
 
+    # Suscribe to classifier notifications
     def classifier_notifications(self):
         self.writeCharacteristic(0x24, b'\x02\x00')
 
+    def set_mode(self, emg_mode, imu_mode, classifier_mode):
+        command_string = struct.pack('<5B', 1, 3, emg_mode, imu_mode, classifier_mode)
+        self.writeCharacteristic(0x19, command_string)
 
 class Device(btle.DefaultDelegate):
     # bluepy functions which receive Bluetooth messages asynchronously,
@@ -45,6 +63,36 @@ class Device(btle.DefaultDelegate):
         btle.DefaultDelegate.__init__(self)
         self.services = Services(mac=get_myo(mac))
         self.services.setDelegate(self)
+
+    def handleNotification(self, cHandle, data):
+        # Notification handles of the 4 EMG data characteristics (raw)
+        if cHandle == 0x2b or cHandle == 0x2e or cHandle == 0x31 or cHandle == 0x34:
+            '''According to http://developerblog.myo.com/myocraft-emg-in-the-bluetooth-protocol/
+            each characteristic sends two secuential readings in each update,
+            so the received payload is split in two samples. According to the
+            Myo BLE specification, the data type of the EMG samples is int8_t.
+            '''
+            emg1 = struct.unpack('<8b', data[:8])
+            emg2 = struct.unpack('<8b', data[8:])
+            print(emg1)
+            print(emg2)
+        # Notification handle of the EMG data characteristic (filtered)
+        elif cHandle == 0x27:
+            emg_filt = struct.unpack('<8H', data[:16])
+            print(emg_filt)
+        # Notification handle of the IMU data characteristic
+        elif cHandle == 0x1c:
+            values = struct.unpack('<10h', data)
+            quat = values[:4]
+            acc = values[4:7]
+            gyro = values[7:10]
+            print(quat)
+        # Notification handle of the battery data characteristic
+        elif cHandle == 0x11:
+            batt = ord(data)
+            print("Battery level: %d" % batt)
+        else:
+            print('data with unknown attr: %02X %s' % (attr, p))
 
 def get_myo(mac=None):
     if mac is not None:
@@ -59,72 +107,19 @@ def get_myo(mac=None):
                 if j[0] == 6 and j[2] == '4248124a7f2c4847b9de04a9010006d5':
                     return str(i.addr).upper()
 
-# From here on, all shit
+class emg_mode:
+    OFF = 0x00
+    FILT = 0x01
+    RAW = 0x02
+    RAW_UNFILT = 0x03
 
-# EMG_HANDLE = 0x27
-#
-# def multichr(ords):
-#     if sys.version_info[0] >= 3:
-#         return bytes(ords)
-#     else:
-#         return ''.join(map(chr, ords))
-#
-# def multiord(b):
-#     if sys.version_info[0] >= 3:
-#         return list(b)
-#     else:
-#         return map(ord, b)
-#
-# def getMyo(mac=None):
-#     if mac is not None:
-#         while True:
-#             for i in btle.Scanner(0).scan(1):
-#                 if i.addr == mac:
-#                     return str(mac).upper()
-#
-#     while True:
-#         for i in btle.Scanner(0).scan(1):
-#             for j in i.getScanData():
-#                 if j[0] == 6 and j[2] == '4248124a7f2c4847b9de04a9010006d5':
-#                     return str(i.addr).upper()
-#
-# def connectMyo(mac=None):
-#     myo = btle.Peripheral(mac)
-#     return myo
-#
-# def firmware(myo):
-#     fw = myo.readCharacteristic(0x17)
-#     fw = struct.unpack('<4h', fw)
-#     return fw
-#
-# def suscribeEmg(myo):
-#     myo.writeCharacteristic(0x2c, b'\x01\x00')
-#     myo.writeCharacteristic(0x2f, b'\x01\x00')
-#     myo.writeCharacteristic(0x32, b'\x01\x00')
-#     myo.writeCharacteristic(0x35, b'\x01\x00')
-#     myo.writeCharacteristic(0x19, b'\x01\x03\x02\x00\x00')
-#
-# class MyoDevice(btle.DefaultDelegate):
-#     def __init__(self, mac=None):
-#         btle.DefaultDelegate.__init__(self)
-#         self.myo = connectMyo(mac = getMyo(mac))
-#         self.myo.setDelegate(self)
-#
-#     def handleNotification(self, cHandle, data):
-#         if cHandle == 0x2b or cHandle == 0x2e or cHandle == 0x31 or cHandle == 0x34:
-#             # '''According to http://developerblog.myo.com/myocraft-emg-in-the-bluetooth-protocol/
-#             # each characteristic sends two secuential readings in each update,
-#             # so the received payload is split in two samples. According to the
-#             # Myo BLE specification, the data type of the EMG samples is int8_t.
-#             # '''
-#             # emg1 = struct.unpack('<8b', pay[:8])
-#             # emg2 = struct.unpack('<8b', pay[8:])
-#             # self.on_emg(emg1, 0)
-#             # self.on_emg(emg2, 0)
-#             emg1 = struct.unpack('<8b', data[:8])
-#             emg2 = struct.unpack('<8b', data[8:])
-#             print(emg1)
-#             print(emg2)
-#
-# class MyoFunctions(object):
-#     pass
+class imu_mode:
+    OFF = 0x00
+    DATA = 0x01
+    EVENTS = 0x02
+    ALL = 0x03
+    RAW = 0x04
+
+class classifier_mode:
+    OFF = 0x00
+    ON = 0x01
